86652c45de93e8df2bc358b6f65d12849349301a
herr kaste <herr.kaste@gmail.com>
2022-10-05 11:38:16 +0200

    Update regex to capture `<col>` if present
    Fixes #6

--- linter.py
+++ linter.py
@@ -18,8 +18,9 @@ class Erlc(Linter):
     tempfile_suffix = "-"
 
     # ERROR FORMAT # <file>:<line>: [Warning:|] <message> #
+    # ERROR FORMAT # <file>:<line>:<col>: [Warning:|] <message> #
     regex = (
-        r".+:(?P<line>\d+):"
+        r"^.+?:(?P<line>\d+):(?:(?P<col>\d+):)?"
         r"(?:(?P<warning>\sWarning:\s)|(?P<error>\s))"
         r"+(?P<message>.+)"
     )
